f11212541fc81b61dbb04c0b8fe9821014ec44d3
harshitsoni01 <soniharshit2000@gmail.com>
2020-07-26 15:53:21 +0530

    added readme

---
+++ Readme.md
@@ -0,0 +1,78 @@
+# Tweet scraper
+
+## Using twitterscraper
+
+### Twitter's API is very hard to work with, and has lots of limitations — luckily this library solves that problem. No API rate limits. No restrictions. Extremely fast
+
+### No developers account or any sort of twitter account required to scrape tweets
+
+### You can scrape tweets of any Twitter user or any hashtags
+
+![Alt text](https://img.shields.io/badge/python-v3.8-g)
+![Alt text](https://img.shields.io/badge/twitterscraper-v1.4.0-yellow)
+
+# Pre-Requisite
+
+- Python v3.8
+- twitterscraper library
+
+# Installing twitterscraper
+
+If you want to use latest version, install from source. To install twitter-scraper from source, follow these steps:
+
+Linux and macOS:
+
+    git clone https://github.com/taspinar/twitterscraper.git
+    cd twitter-scraper
+    sudo python3 setup.py install 
+Also, you can install with PyPI.
+
+    pip install twitterscraper
+
+# Output
+
+### Per Tweet it scrapes the following information:
+
+- Tweet_id
+- Tweet_url
+- Tweet text
+- Tweet html
+- Links inside Tweet
+- Hashtags inside Tweet
+- Image URLS inside Tweet
+- Video URL inside Tweet
+- Tweet timestamp
+- Tweet Epoch timestamp
+- Tweet No. of likes
+- Tweet No. of replies
+- Tweet No. of retweets
+- Username
+- User Full Name / Screen Name
+- User ID
+- Tweet is an reply to
+- Tweet is replied to
+- List of users Tweet is an reply to
+- Tweet ID of parent tweet
+
+### In addition it can scrape for the following user information:
+
+- Date user joined
+- User location (if filled in)
+- User blog (if filled in)
+- User No. of tweets
+- User No. of following
+- User No. of followers
+- User No. of likes
+- User No. of lists
+- User is verified
+
+## Output files:
+
+All of the retrieved Tweets are stored in the indicated output file that maybe csv, json or anyother file type.
+
+The output files can be stored and opened using pandas dataframe
+
+    import pandas as pd
+    df = pd.DataFrame(b.__dict__ for b in tweets)
+
+
--- tweet_scraper.py
+++ tweet_scraper.py
@@ -6,7 +6,7 @@ import re
 
 #set date according to your preference
 #the tweets will be scraped a day prior than the end_date
-begin_date = dt.date(2020,7,26)
+begin_date = dt.date(2020,7,25)
 end_date = dt.date.today()
 
 #enter the amount of tweets you want 
@@ -17,20 +17,26 @@ limits = int(limits)
 #to get tweets in english
 langs = "english"
 
+user1 = "PolitiFact"
+user2 = "GossipCop"
+
 #to get the tweets
 basic_tweets = query_tweets("#news" , begindate=begin_date, enddate=end_date , limit=limits, lang=langs)
-user1_tweets = query_tweets("@PolitiFact" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
-user2_tweets = query_tweets("@GossipCop" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
+user1_tweets = query_tweets(user1 , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
+user2_tweets = query_tweets(user2 , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
 
 #to store the tweets in csv files using pandas
 df = pd.DataFrame(b.__dict__ for b in basic_tweets)
 df.drop(df.columns.difference(['username','text']), 1, inplace=True)
-df.to_csv("C:/fake_news_detection/csv_files/tweet.csv", index= False)
+#to get only username and text of a tweet
+df.to_csv("C:/tweet_scraper/tweets/tweet.csv", index= False)
 
 df1 = pd.DataFrame(u1.__dict__ for u1 in user1_tweets)
-df1.drop(df1.columns.difference(['username','text']), 1, inplace=True)
-df1.to_csv("C:/fake_news_detection/csv_files/politifact.csv" , index= False)
+#to get everything from the tweet i.e 
+# username, text, retweet, time, likes,etc
+df1.to_csv("C:/tweet_scraper/tweets/politifact.csv" , index= False)
 
 df2 = pd.DataFrame(u2.__dict__ for u2 in user2_tweets)
-df2.drop(df2.columns.difference(['username','text']), 1, inplace=True)
-df2.to_csv("C:/fake_news_detection/csv_files/gossipcop.csv" , index= False)
+#you can also store the data in json format just by replacing csv by json
+#but first you need to create a file named xyz.csv/xyz.json
+df2.to_csv("C:/tweet_scraper/tweets/gossipcop.csv" , index= False)
